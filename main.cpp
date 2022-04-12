c67f655dce5890ab98054a6b76c14e1c2255d388
Bence Gercuj <bencegercuj@gmail.com>
2022-04-12 15:40:54 +0200

    Widgets are now shown

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include "graphics.hpp"
 #include "widgetbase.hpp"
 #include "windowbase.hpp"
+#include "window.hpp"
 
 int main()
 {
+    genv::gout.open(400, 400);
+
+    Window w;
+    w.event_loop();
+
     return 0;
 }

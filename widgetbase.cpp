e3ff197d15f1effef6f253dab12fc91d4cc342c2
Bence Gercuj <bencegercuj@gmail.com>
2022-04-12 15:41:58 +0200

    Added focus var

--- widgetbase.cpp
+++ widgetbase.cpp
@@ -1,5 +1,6 @@
 #include "widgetbase.hpp"
+#include "windowbase.hpp"
 
-WidgetBase::WidgetBase(WindowBase *mainw, int posx, int posy, int sizex, int sizey, std::string name) : _mainw(mainw), _posx(posx), _posy(posy), _sizex(sizex), _sizey(sizey), _name(name) { }
+WidgetBase::WidgetBase(WindowBase *mainw, int posx, int posy, int sizex, int sizey, std::string name) : _mainw(mainw), _posx(posx), _posy(posy), _sizex(sizex), _sizey(sizey), _name(name), _focused(false) { mainw->add_widget(this); }
 
 WidgetBase::~WidgetBase() { }

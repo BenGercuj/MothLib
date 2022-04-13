c667b1d56bb5aaaff29c719d9ffbf8f73b4a49d9
Bence Gercuj <bencegercuj@gmail.com>
2022-04-13 12:10:39 +0200

    Added spinners to spinbox

--- spinbox.cpp
+++ spinbox.cpp
@@ -9,8 +9,31 @@ bool SpinBox::is_selected(int mx, int my)
     return mx <= _posx+_sizex && mx >= _posx && my >= _posy-_sizey && my <= _posy;
 }
 
+int SpinBox::selected_spinner(int mx, int my)
+{ // 0: none, 1: lower, 2: upper
+
+    // Hitbox debug
+//    gout << color(0, 100, 0) << move_to(_posx+_sizex-10, _posy-(_sizey/2)) << box_to(_posx+_sizex+2, _posy);
+//    gout << color(100, 0, 0) << move_to(_posx+_sizex-10, _posy-(_sizey/2)-4) << box_to(_posx+_sizex+2, _posy-_sizey-2);
+
+    if (mx >= _posx+_sizex-10 && mx <= _posx+_sizex+2)
+    {
+        if (my <= _posy && my >= _posy-(_sizey/2))
+        {
+            return 1;
+        }
+
+        else if (my >= _posy-_sizey-2 && my <= _posy-(_sizey/2)-4)
+        {
+            return 2;
+        }
+    }
+
+    return 0;
+}
+
 void SpinBox::draw()
-{
+{ // Creator's note: I genuinely hope I will /not/ have to write GUI code like this in the upcoming.. I dunno, SIXTY YEARS
     gout << color(0, 0, 0) << move_to(_posx-2, _posy+2) << box(_sizex+9, -_sizey-9);
 
     if (_focused)
@@ -23,7 +46,17 @@ void SpinBox::draw()
         gout << color(255, 255, 255) << move_to(_posx, _posy) << box(_sizex+5, -_sizey-5) << move_to(_posx+2, _posy-2) << color(0, 0, 0) << box(_sizex+1, -_sizey-1) << color(255, 255, 255);
     }
 
+    // Horizontal spinner separator
     gout << move_to(_posx+_sizex-10, _posy) << line(0, -_sizey-5);
+
+    // Bottom spinner arrow
+    gout << move_to(_posx+_sizex-10, _posy-(_sizey/2)) << line_to(_posx+_sizex-3, _posy-1) << line_to(_posx+_sizex+2, _posy-(_sizey/2)+1);
+
+    // Upper spinner arrow
+    gout << move_to(_posx+_sizex-10, _posy-(_sizey/2)) << line_to(_posx+_sizex-3, _posy-_sizey-2) << line_to(_posx+_sizex+2, _posy-(_sizey/2)-3);
+
+    // Vertical spinner separator
+    gout << move_to(_posx+_sizex-10, _posy-(_sizey/2)-2) << line_to(_posx+_sizex+3, _posy-(_sizey/2)-2);
 
     gout << move_to(_posx+2, _posy-(_sizey/2)+2) << text(std::to_string(_val));
 
@@ -42,5 +75,11 @@ void SpinBox::event_handler(event ev)
 
     else if (ev.keycode == key_pgdn && _val-10 >= _minval) { _val -= 10; }
 
+    else if (ev.button == btn_left)
+    {
+        if (selected_spinner(ev.pos_x, ev.pos_y) == 1 && _val > _minval) { _val--; }
+
+        else if (selected_spinner(ev.pos_x, ev.pos_y) == 2 && _val < _maxval) { _val++; }
+    }
 
 }
--- spinbox.hpp
+++ spinbox.hpp
@@ -11,9 +11,9 @@ class SpinBox : public WidgetBase
         SpinBox(WindowBase *mainw, int posx, int posy, int sizex, int sizey, std::string name, int val, int minval, int maxval);
 
         bool is_selected(int mx, int my);
+        int selected_spinner(int mx, int my);
         void draw();
         void event_handler(genv::event ev);
-        void selected_spinner(int mx, int my);
 };
 
 #endif // SPINBOX_HPP

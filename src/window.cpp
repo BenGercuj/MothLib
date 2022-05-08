a795dcb6da174a4837af0a7295aca6674568e7cc
Bence Gercuj <bencegercuj@gmail.com>
2022-05-08 23:45:35 +0200

    Added.. b u t t o n s

--- src/window.cpp
+++ src/window.cpp
@@ -2,12 +2,28 @@
 
 Window::Window(int winX, int winY) : WindowBase(winX, winY)
 {
-    std::function<void()> f = [this]()
-    {
+    //statictext at y=YY/5
+    btn = new Button(this, 6+(3*XX/4), YY/5+4, XX/4-12, YY/5, "btn_div", "/", [](){});
 
-    };
+    btn = new Button(this, 2, 2*YY/5, XX/4, YY/5, "btn_7", "7", [](){});
+    btn = new Button(this, 2, 3*YY/5, XX/4, YY/5, "btn_4", "4", [](){});
+    btn = new Button(this, 2, 4*YY/5, XX/4, YY/5, "btn_1", "1", [](){});
+    btn = new Button(this, 2, ((5*YY)/5)-3, XX/4, YY/5, "btn_sign", "-/+", [](){});
 
+    btn = new Button(this, 2+(1*XX/4), 2*YY/5, XX/4, YY/5, "btn_8", "8", [](){});
+    btn = new Button(this, 2+(1*XX/4), 3*YY/5, XX/4, YY/5, "btn_5", "5", [](){});
+    btn = new Button(this, 2+(1*XX/4), 4*YY/5, XX/4, YY/5, "btn_2", "2", [](){});
+    btn = new Button(this, 2+(1*XX/4), ((5*YY)/5)-3, XX/4, YY/5, "btn_0", "0", [](){});
 
+    btn = new Button(this, 2+(2*XX/4), 2*YY/5, XX/4, YY/5, "btn_9", "9", [](){});
+    btn = new Button(this, 2+(2*XX/4), 3*YY/5, XX/4, YY/5, "btn_6", "6", [](){});
+    btn = new Button(this, 2+(2*XX/4), 4*YY/5, XX/4, YY/5, "btn_3", "3", [](){});
+    btn = new Button(this, 2+(2*XX/4), ((5*YY)/5)-3, XX/4, YY/5, "btn_del", "DEL", [](){});
+
+    btn = new Button(this, 6+(3*XX/4), 2*YY/5, XX/4-12, YY/5, "btn_multi", "*", [](){});
+    btn = new Button(this, 6+(3*XX/4), 3*YY/5, XX/4-12, YY/5, "btn_sub", "-", [](){});
+    btn = new Button(this, 6+(3*XX/4), 4*YY/5, XX/4-12, YY/5, "btn_add", "+", [](){});
+    btn = new Button(this, 6+(3*XX/4), ((5*YY)/5)-3, XX/4-12, YY/5, "btn_equ", "=", [](){});
 
 //    sb = new SpinBox(this, 250, 200, 50, 25, "spinbox1", 10, 0, 20);
 //    sb = new SpinBox(this, 175, 300, 50, 25, "spinbox2", 13, -666, 333);

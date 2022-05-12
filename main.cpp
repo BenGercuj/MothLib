b7fdabbacf1348ee4370bb9dafe1703a97c092d7
Bence Gercuj <bencegercuj@gmail.com>
2022-05-12 10:47:23 +0200

    hih.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ const int YY = 400;
 // Egy-egy ablak a Window child-ja legyen, pl AutoKolcsonzo
 // Adatszerk tervezésénél legyen bolondbiztos ez a szar
 // FONTOS! Vizsga el?tt bugmentesítsd (ha kell írd újra) és írj további widgeteket mer ha nem cumi lesz
+// ListBox, Dropmenu, stb ÍRD MEEEEEEG
 
 int main()
 {

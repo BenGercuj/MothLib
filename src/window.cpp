c7086d8b3e50e964528129d7bb414b1c798782cc
Bence Gercuj <bencegercuj@gmail.com>
2022-05-19 10:24:18 +0200

    SpinBox upper button hitbox screwed, FIX IT

--- src/window.cpp
+++ src/window.cpp
@@ -6,4 +6,5 @@ Window::Window(int winX, int winY) : WindowBase(winX, winY)
 {
     btn = new Button(this, 100, 100, 100, 50, "btn_demo", "DEMO", [](){ std::cout << "demo pressed\n"; }); // seems to work
     lb = new ListBox(this, 50, 250, 100, 100, "lb_demo", { "egyeske", "ketteske", "harmaska", "hihi", "lajos", "lajoska" });
+    sb = new SpinBox(this, 175, 200, 50, 50, "sb_demo", 0, -10, -5);
 }
